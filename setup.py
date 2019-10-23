982ad5d4d8240ff5f09312356ff48bf79133267d
skilful-sovereign <xlr80000@gmail.com>
2019-10-24 00:02:45 +0200

    added support for Google Chrome drivers for selenium added argument to start.py to force credential creation/update

--- setup.py
+++ setup.py
@@ -6,11 +6,9 @@ cmd0 = 'sudo safaridriver --enable'
 cmd1 = 'sudo easy_install pip'
 cmd2 = 'sudo pip install --upgrade pip'
 cmd3 = 'sudo pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib yamlreader selenium'
-# run autocred.py to get credentials
-cmd4 = 'python autocred.py'
+
 
 os.system(cmd0)
 os.system(cmd1)
 os.system(cmd2)
 os.system(cmd3)
-os.system(cmd4)
--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 import hoursAndWageCalculator
 import gmail
 import argparse
+import autocred
+import os
 # FILE_NAME must be the same as filename on Google Drive
 FILE_NAME = "calculator_data.yml"
 
@@ -14,7 +16,15 @@ def start():
     parser.add_argument("start", help="Start date as dd.mm.yyyy", nargs='?', default=None)
     parser.add_argument("end", help="End date as dd.mm.yyyy", nargs='?', default=None)
     parser.add_argument("-e", "-email", help="Option to send email", action='store_true')
+    parser.add_argument("-n", "-new", help="Option to make new credentials", action='store_true')
     args = parser.parse_args()
+
+    if args.n or not(os.path.exists("credentials.json")):
+        if os.path.exists("credentials.json"):
+            os.remove("credentials.json")
+        if os.path.exists("credentials.pickle"):
+            os.remove("credentials.pickle")
+        autocred.autocred()
 
     msgEmail, msgPrint = hoursAndWageCalculator.getHoursAndWages(args.start, args.end)
 
@@ -22,7 +32,6 @@ def start():
 
     if args.e:
         gmail.generateAndSendEmail(msgEmail)
-
 
 
 if __name__ == '__main__':
